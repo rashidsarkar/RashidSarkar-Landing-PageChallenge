4fa6d073e3af2a353ad0b842474cdc7c92f18469
rashidsarkar <rashidrock558@gmail.com>
2024-05-20 13:32:01 +0600

    "Updated Banner component with new layout, added cart icon and buy now button"

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,33 @@
 import oval from "../../assets/Oval.png";
 import BannerImage from "../../assets/BannerImage.png";
+import { FaCartShopping } from "react-icons/fa6";
 
 function Banner() {
   return (
     <div className="bg-[#EAEAEA]">
-      <div className="flex flex-col items-center justify-center h-[100vh]">
-        <div className="relative items-center justify-center bgShape">
-          <img className="absolute mx-auto" src={oval} alt="" />
-          <div className=" w-[500px]">
+      <div className="flex flex-col lg:flex-row-reverse items-center justify-center h-auto  lg:h-[100vh]">
+        <div className="relative flex items-center justify-center w-full lg:mt-30 bgShape lg:w-auto">
+          <img
+            className="absolute inset-0 object-cover w-full h-full mx-auto lg:w-auto lg:h-auto"
+            src={oval}
+            alt=""
+          />
+          <div className="w-4/5 md:w-3/5 lg:w-[500px] relative">
             <img
-              className="relative bottom-0 w-full left-10 top-10"
+              className="relative w-full lg:w-auto lg:left-10 left-5 lg:top-10"
               src={BannerImage}
               alt=""
             />
           </div>
+        </div>
+        <div className="p-4 space-y-8 text-center lg:text-left lg:absolute lg:left-52 textPart lg:p-0">
+          <h1 className="text-2xl font-bold md:text-3xl lg:text-4xl text-text-primary">
+            Personalized Eyeglass <br /> Shopping
+          </h1>
+          <button className="flex items-center justify-center px-4 py-2 mx-auto space-x-2 text-white rounded-md lg:mx-0 btn bg-text-primary hover:bg-text-primary">
+            <FaCartShopping />
+            <span>Buy Now</span>
+          </button>
         </div>
       </div>
     </div>
